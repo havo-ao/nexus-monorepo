95a91616688db3535ecf3e8670a311cffc2d9bb2
alerojas-2005 <alexamorato5@gmail.com>
2026-05-18 17:01:55 -0500

    fix: prettier formatting issues

--- services/portfolio-service/src/valuations/valuations.module.ts
+++ services/portfolio-service/src/valuations/valuations.module.ts
@@ -4,11 +4,7 @@ import { MarketQuotesClient } from './clients/market-quotes.client';
 import { ValuationsService } from './services/valuations.service';
 
 @Module({
-  providers: [
-    MarketInstrumentsClient,
-    MarketQuotesClient,
-    ValuationsService,
-  ],
+  providers: [MarketInstrumentsClient, MarketQuotesClient, ValuationsService],
   exports: [ValuationsService],
 })
 export class ValuationsModule {}
