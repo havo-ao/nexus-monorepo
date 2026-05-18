2891ff1d683d8f22918925275d6f8a1f1c75f0be
DaRaM1025 <davidsanty.ramireza@gmail.com>
2026-05-18 15:22:34 -0500

    feat: add model classes

---
+++ services/identity-service/src/main/java/com/nexus/identityservice/config/StripeProperties.java
@@ -0,0 +1,13 @@
+package com.nexus.identityservice.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+@Data
+@Component
+@ConfigurationProperties(prefix = "stripe")
+public class StripeProperties {
+    private String secretKey;
+    private String monthlyPriceId;
+    private String yearlyPriceId;
+}
